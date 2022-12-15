fc6d7271960ed66dd52f29c3e53dba584e44db3c
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-12-15 11:12:29 +0700

    feat: add cn func to merge clsx with twmerge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { cn } from '#/lib/utils';
 
 export default function Home() {
   return (
     <div>
       <main>
-        <h1 className="text-red-600 font-bold">Hai</h1>
+        <h1 className={cn(' text-red-500')}>Hai</h1>
       </main>
     </div>
   );
