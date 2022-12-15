d985360b68e0eb83840ab57d15592e3bb9826c00
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-12-15 10:48:09 +0700

    feat: add deafult seo

---
+++ _content/next-seo.config.ts
@@ -0,0 +1,13 @@
+const defaultSEOConfig = {
+  defaultTitle: 'QR Generator',
+  description: 'A simple web app to generate the QR Code.',
+  openGraph: {
+    url: 'https://jenxQ.vercel.app.com',
+    title: 'QR Generator',
+    description: 'A simple web app to generate the QR Code.',
+    type: 'website',
+    site_name: 'JENXQ',
+  },
+};
+
+export default defaultSEOConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
+import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 import React from 'react';
+import defaultSEOConfig from '_content/next-seo.config';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <DefaultSeo {...defaultSEOConfig} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
